028a5ae45640f29edd484b280019c1d4429036fe
Noah Nielsen <noahanielsen@gmail.com>
2024-12-03 10:22:53 +0100

    Added repository

---
+++ source/DrawAndGuess/DrawAndGuess.DataAccess/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawAndGuess.DataAccess
+{
+    public interface IRepository<T>
+    {
+        Task Add(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(int id);
+
+        Task<T> GetById(int id);
+
+        Task<IEnumerable<T>> GetAll();
+    }
+}
---
+++ source/DrawAndGuess/DrawAndGuess.DataAccess/Repository.cs
@@ -0,0 +1,41 @@
+﻿namespace DrawAndGuess.DataAccess
+{
+    public class Repository<T>(DataContext context) : IRepository<T> where T : class
+    {
+        protected readonly DataContext _context = context;
+
+        public async Task Add(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await GetById(id);
+            await Delete(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
