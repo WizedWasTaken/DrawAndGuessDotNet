54a95fa932f78fd92474353d4a4b53293e34abe4
Noah Nielsen <noahanielsen@gmail.com>
2024-12-12 23:57:34 +0100

    Added tests

---
+++ source/DrawAndGuess/DrawAndGuess.Client/client/__tests__/lobbies.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen, waitFor, act } from "@testing-library/react";
+import Home from "@/app/lobbies/page";
+import { LobbiesTable } from "@/components/dataTable/Lobbies";
+
+jest.mock("@/components/dataTable/Lobbies", () => ({
+  LobbiesTable: jest.fn(() => <div>Lobbies Table Mock</div>),
+}));
+
+describe("Home Page", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders the Home component correctly", async () => {
+    await act(async () => {
+      render(<Home />);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText("Lobbies Table Mock")).toBeInTheDocument();
+    });
+  });
+
+  it("contains the LobbiesTable component", async () => {
+    await act(async () => {
+      render(<Home />);
+    });
+
+    await waitFor(() => {
+      expect(LobbiesTable).toHaveBeenCalled();
+    });
+  });
+});
