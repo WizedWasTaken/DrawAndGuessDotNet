9f89a8fb9f852dcf01725601f8e2cedf89ef79c9
Noah Nielsen <noahanielsen@gmail.com>
2024-12-14 02:27:20 +0100

    Fixed auth provider

--- source/DrawAndGuess/DrawAndGuess.Client/client/src/app/layout.tsx
+++ source/DrawAndGuess/DrawAndGuess.Client/client/src/app/layout.tsx
@@ -3,10 +3,18 @@ import "./globals.scss";
 
 // Contexts
 import { SignalRProvider } from "@/lib/contexts/SignalRContext";
+
+// Providers
 import { ThemeProvider } from "@/lib/providers/ThemeProvider";
+import AuthProvider from "@/lib/providers/AuthProvider";
+
+// Components
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import TechnicalInformation from "@/components/TechnicalInformation";
+
+// Toaster
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Tegn og gæt",
@@ -23,23 +31,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased min-h-screen flex flex-grow flex-col dark`}>
-        <SignalRProvider url={SIGNALR_URL}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
-            <main className="flex flex-grow flex-col">{children}</main>
-            <Footer />
-            <div className="fixed bottom-0 p-5">
-              <TechnicalInformation />
-            </div>
-          </ThemeProvider>
-        </SignalRProvider>
+    <html lang="en" suppressHydrationWarning style={{ colorScheme: "dark" }}>
+      <body
+        className={`bg-gradient-to-br from-background to-secondary antialiased min-h-screen flex flex-grow flex-col`}
+      >
+        <AuthProvider>
+          <SignalRProvider url={SIGNALR_URL}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Header />
+              <main className="flex flex-grow flex-col">{children}</main>
+              <Footer />
+              <div className="fixed bottom-0 p-5">
+                <TechnicalInformation />
+              </div>
+              <Toaster />
+            </ThemeProvider>
+          </SignalRProvider>
+        </AuthProvider>
       </body>
     </html>
   );
