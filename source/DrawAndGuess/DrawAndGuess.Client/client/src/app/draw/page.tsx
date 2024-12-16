af2b0d9d70e518fe75454be5e60cca10a3fe1d95
WizedWasTaken <141151679+WizedWasTaken@users.noreply.github.com>
2024-12-16 11:39:04 +0100

    Added canvas

---
+++ source/DrawAndGuess/DrawAndGuess.Client/client/src/app/draw/page.tsx
@@ -0,0 +1,9 @@
+import Canvas from "@/components/canvas/Canvas"
+
+export default function CanvasPage() {
+    return (
+        <div className="container flex flex-col flex-grow mx-auto">
+            <Canvas />
+        </div>
+    );
+}
---
+++ source/DrawAndGuess/DrawAndGuess.Client/client/src/components/canvas/MultiplayerCanvas.tsx
@@ -0,0 +1,9 @@
+import Canvas from "@/components/canvas/Canvas";
+
+export default function MultiplayerCanvas() {
+    return (
+        <section className="flex flex-grow flex-col">
+            <Canvas />
+        </section>
+    );
+}
